424549f0c464b361722d327a7fdae289bbd332b4
girish chalumuri <girishchalumuri@gmail.com>
2023-03-26 08:35:37 +0530

    init nhost

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,21 @@
-import '../styles/globals.css';
-import { Toaster } from 'react-hot-toast';
-import { UserProvider } from '../UserProvider';
+import "../styles/globals.css";
+import { Toaster } from "react-hot-toast";
+import { UserProvider } from "../UserProvider";
+import { NhostProvider, NhostClient } from "@nhost/nextjs";
+
+const nhost = new NhostClient({
+  subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN || "",
+  region: process.env.NEXT_PUBLIC_NHOST_REGION || "",
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-      <Toaster />
-    </UserProvider>
+    <NhostProvider nhost={nhost} initial={pageProps.nhostSession}>
+      <UserProvider>
+        <Component {...pageProps} />
+        <Toaster />
+      </UserProvider>
+    </NhostProvider>
   );
 }
 
